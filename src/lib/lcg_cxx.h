c73f7cc2e3339f1e60f5947084dbbe0212df8a37
Yi Zhang <zhangyiss@icloud.com>
2021-02-01 22:16:45 +0800

    add lcg_cxx

---
+++ src/lib/lcg_cxx.h
@@ -0,0 +1,93 @@
+/******************************************************//**
+ *    C/C++ library of linear conjugate gradient.
+ *
+ * Copyright (c) 2019-2029 Yi Zhang (zhangyiss@icloud.com)
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *********************************************************/
+
+#ifndef _LCG_CXX_H
+#define _LCG_CXX_H
+
+#include "lcg.h"
+#include "iostream"
+
+class LCG_Solver
+{
+protected:
+	lcg_para param_;
+
+public:
+	LCG_Solver()
+	{
+		param_ = lcg_default_parameters();
+	}
+
+	~LCG_Solver(){}
+
+	static void _Ax(void* instance, const lcg_float* a, lcg_float* b, const int num)
+	{
+		return reinterpret_cast<LCG_Solver*>(instance)->Ax(a, b, num);
+	}
+	virtual void Ax(const lcg_float* a, lcg_float* b, const int num) = 0;
+
+	static int _Progress(void* instance, const lcg_float* m, const lcg_float converge, 
+		const lcg_para *param, const int n_size, const int k)
+	{
+		return reinterpret_cast<LCG_Solver*>(instance)->Progress(m, converge, param, n_size, k);
+	}
+	virtual int Progress(const lcg_float* m, const lcg_float converge, 
+		const lcg_para *param, const int n_size, const int k)
+	{
+		std::clog << "Iteration-times: " << k << "\tconvergence: " << converge << std::endl;
+#ifdef __linux__
+		if (converge > param->epsilon) std::clog << "\033[1A\033[K";
+#elif defined __APPLE__
+		if (converge > param->epsilon) std::clog << "\033[1A\033[K";
+#endif
+		return 0;
+	}
+
+	void set_lcg_parameter(const lcg_para &in_param)
+	{
+		param_ = in_param;
+		return;
+	}
+
+	void Minimize(lcg_float *m, const lcg_float *b, int x_size, 
+		lcg_solver_enum solver_id = LCG_CG, const lcg_float *p = NULL)
+	{
+		// 使用lcg求解 注意当我们使用函数指针来调用求解函数时默认参数不可以省略
+		int ret = lcg_solver(_Ax, _Progress, m, b, x_size, &param_, this, solver_id, p);
+		if (ret < 0) std::cout << lcg_error_str(ret) << std::endl;
+		return;
+	}
+
+	void Minimize_Constrained(lcg_float *m, const lcg_float *b, const lcg_float* low, 
+		const lcg_float *hig, int x_size, lcg_solver_enum solver_id = LCG_CG)
+	{
+		// 使用lcg求解 注意当我们使用函数指针来调用求解函数时默认参数不可以省略
+		int ret = clcg_solver(_Ax, _Progress, m, b, low, hig, x_size, &param_, this, solver_id);
+		if (ret < 0) std::cout << lcg_error_str(ret) << std::endl;
+		return;
+	}
+};
+
+#endif //_LCG_CXX_H
---
+++ src/sample/sample5.cpp
@@ -0,0 +1,80 @@
+#include "../lib/lcg_cxx.h"
+#include "iostream"
+
+using std::cout;
+using std::clog;
+using std::endl;
+
+class TESTFUNC : public LCG_Solver
+{
+public:
+	TESTFUNC();
+	~TESTFUNC(){}
+	void Ax(const lcg_float* a, lcg_float* b, const int num); //定义共轭梯度中Ax的算法
+
+private:
+	lcg_float kernel_[3][3];
+};
+
+TESTFUNC::TESTFUNC()
+{
+	// 测试线性方程组
+	// 6.3*x1 + 3.9*x2 + 2.5*x3 = -2.37
+	// 3.9*x1 + 1.2*x2 + 3.1*x3 = 5.82
+	// 2.5*x1 + 3.1*x2 + 7.6*x3 = 5.21
+	// 目标解 x1=1.2 x2=-3.7 x3=1.8
+	// 注意根据共轭梯度法的要求 kernel是一个N阶对称阵
+	kernel_[0][0] = 6.3; kernel_[0][1] = 3.9; kernel_[0][2] = 2.5;
+	kernel_[1][0] = 3.9; kernel_[1][1] = 1.2; kernel_[1][2] = 3.1;
+	kernel_[2][0] = 2.5; kernel_[2][1] = 3.1; kernel_[2][2] = 7.6;
+}
+
+void TESTFUNC::Ax(const lcg_float* a, lcg_float* b, const int num)
+{
+	for (int i = 0; i < num; i++)
+	{
+		b[i] = 0.0;
+		for (int j = 0; j < num; j++)
+		{
+			b[i] += kernel_[i][j]*a[j];
+		}
+	}
+	return;
+}
+
+int main(int argc, char const *argv[])
+{
+	lcg_float* m;
+	lcg_float* b;
+	lcg_float* p;
+	// 初始解
+	m = lcg_malloc(3);
+	m[0] = 0.0; m[1] = 0.0; m[2] = 0.0;
+	// 拟合目标值（含有一定的噪声）
+	b = lcg_malloc(3);
+	b[0] = -2.3723; b[1] = 5.8201; b[2] = 5.2065;
+	// 测试预优矩阵 这里只是测试流程 预优矩阵值全为1 并没有什么作用
+	p = lcg_malloc(3);
+	p[0] = p[1] = p[2] = 1.0;
+
+	TESTFUNC test;
+	test.Minimize(m, b, 3, LCG_CG);
+	// 输出解
+	for (int i = 0; i < 3; i++)
+	{
+		cout << m[i] << endl;
+	}
+
+	m[0] = 0.0; m[1] = 0.0; m[2] = 0.0;
+	test.Minimize(m, b, 3, LCG_PCG, p);
+	// 输出解
+	for (int i = 0; i < 3; i++)
+	{
+		cout << m[i] << endl;
+	}
+
+	lcg_free(m);
+	lcg_free(b);
+	lcg_free(p);
+	return 0;
+}
